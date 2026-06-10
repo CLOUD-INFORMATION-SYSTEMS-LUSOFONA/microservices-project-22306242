e6f35b6b537a0b640f755f2ba0e84af239b7b704
goncSoares <a22306242@alunos.ulht.pt>
2026-06-10 20:58:09 +0100

    feat: add temporary SSH access for GitHub Actions runner in CI/CD

--- infrastructure/terraform/environments/dev/outputs.tf
+++ infrastructure/terraform/environments/dev/outputs.tf
@@ -5,3 +5,4 @@ output "order_ip"     { value = module.compute.order_ip }
 output "rds_endpoint" { value = module.db.rds_endpoint }
 output "sqs_queue_url" { value = module.queue.queue_url }
 output "sqs_dlq_url"  { value = module.queue.dlq_url }
+output "app_sg_id" { value = module.compute.app_sg_id }
